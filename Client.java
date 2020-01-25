05e4286b9dfbb827d792a6d66206698239797749
Burple-ML <raghav.patne1@gmail.com>
2020-01-25 12:19:56 +0100

    Added all code with report

---
+++ Client.java
@@ -0,0 +1,9 @@
+import java.rmi.*;
+public class Client{
+ 	public static void main(String a[]) throws Exception
+ 	{
+ 		AddI obj = (AddI)Naming.lookup("ADD");
+ 		int n = obj.add(5,4);
+ 		System.out.println("the addition is " + n);
+ 	}
+}
